ba857f855059883e423ac3caebd4417711935642
Adam Michael <adam@ajmichael.net>
2015-10-25 09:50:11 -0400

    Added Google Maps and Pathfinder set up to sample application.

--- examples/ClusterObserver/ClusterObserver/ViewController.swift
+++ examples/ClusterObserver/ClusterObserver/ViewController.swift
@@ -7,19 +7,96 @@
 //
 
 import UIKit
+import Pathfinder
 
 class ViewController: UIViewController {
+  let pathfinderAppId: String = "abc"
+  let userCredentials: String = "abc"
+
+  var cluster: Cluster!
+
+  @IBOutlet weak var mapView: GMSMapView!
+
+  let locationManager = CLLocationManager()
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Set up Pathfinder
+    cluster = Pathfinder(applicationIdentifier: pathfinderAppId, userCredentials: userCredentials).defaultCluster()
+    cluster.delegate = self
+    cluster.connect()
+
+    // Set up Google Maps
+    mapView.delegate = self
+    mapView.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
+
+    // Request device location
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+}
 
+// MARK: - CLLocationManagerDelegate
+extension ViewController: CLLocationManagerDelegate {
+
+  func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+      mapView.myLocationEnabled = true
+    }
+  }
 
 }
 
+// MARK: - GMSMapViewDelegate
+extension ViewController: GMSMapViewDelegate {
+
+  override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    let myLocation: CLLocation = change![NSKeyValueChangeNewKey] as! CLLocation
+    mapView.camera = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 10.0)
+    mapView.settings.myLocationButton = true
+  }
+  
+}
+
+// MARK: - ClusterDelegate
+extension ViewController: ClusterDelegate {
+
+  func connectedTo(cluster: Cluster) {
+
+  }
+
+  func vehicleDidComeOnline(vehicle: Vehicle) {
+
+  }
+
+  func vehicleDidGoOffline(vehicle: Vehicle) {
+
+  }
+
+  func commodityWasRequested(commodity: Commodity) {
+
+  }
+
+  func commodityWasPickuped(commodity: Commodity) {
+
+  }
+
+  func commodityWasDroppedOff(commodity: Commodity) {
+
+  }
+
+  func commodityWasCancelled(c: Commodity) {
+
+  }
+
+  func clusterWasRouted(routes: [Route]) {
+    
+  }
+
+}
