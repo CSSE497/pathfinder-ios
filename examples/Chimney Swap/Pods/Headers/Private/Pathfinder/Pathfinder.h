8a676bcecde00d89251c1eabcccd4bc58d8bc65e
Adam Michael <adam@ajmichael.net>
2015-10-25 23:58:36 -0400

    Started Vehicle sample application.

---
+++ examples/Chimney Swap/Pods/Headers/Private/Pathfinder/Pathfinder.h
@@ -0,0 +1,1 @@
+../../../../../../framework/Pathfinder/Pathfinder.h
---
+++ examples/Chimney Swap/Pods/Target Support Files/Pathfinder/Pathfinder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pathfinder : NSObject
+@end
+@implementation PodsDummy_Pathfinder
+@end
